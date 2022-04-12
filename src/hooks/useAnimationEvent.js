e3ee63f34d0415c6316f3891c399a31768531500
wangliang000 <wl122122@163.com>
2022-04-13 02:45:35 +0800

    fix: removeListtener

--- src/hooks/useAnimationEvent.js
+++ src/hooks/useAnimationEvent.js
@@ -1,24 +1,29 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 export function useAnimationEvents(doms, animationEvent, handle) {
   const listenDoms = useRef([]);
+  const [isAddListener, setIsAddEventListener] = useState(false);
+  const handleFn = useCallback(() => {
+    handle();
+    setIsAddEventListener(false);
+  }, []);
   useEffect(() => {
     listenDoms.current = doms.filter((_d) => isHTMLElement(_d));
+    if (isAddListener) {
+      listenDoms.current.lenth &&
+        listenDoms.current.forEach((_d) =>
+          _d.removeEventListener(animationEvent, handleFn),
+        );
+    }
+  }, [doms, isAddListener, animationEvent, handleFn]);
 
-  }, [doms]);
   useEffect(() => {
     if (listenDoms.current.length) {
-      
       listenDoms.current.forEach((_d) =>
-        _d.addEventListener(animationEvent, handle),
+        _d.addEventListener(animationEvent, handleFn),
       );
+      setIsAddEventListener(true);
     }
-    // return () => {
-    //   listenDoms.current.lenth &&
-    //     listenDoms.current.forEach((_d) =>
-    //       _d.removeEventListener(animationEvent, handle),
-    //     );
-    // };
-  }, [animationEvent, handle]);
+  }, [animationEvent, handle, handleFn]);
 }
 
 function isHTMLElement(dom = null) {
